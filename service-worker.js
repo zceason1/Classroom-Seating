787f27b4c1f826cbf3037ca8e9d93867a19f47d2
zceason1 <135088235+zceason1@users.noreply.github.com>
2026-06-22 15:21:51 +0800

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'classroom-seating-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './service-worker.js',
+  './src/app.js',
+  './src/core.js',
+  './src/styles.css',
+  './assets/icon.svg',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(
+      keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)),
+    )),
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request)),
+  );
+});
